b103bc7703bd5a1cdd153658b2eeef303842a88a
Helma Torkamaan <h.torkamaan@gmail.com>
2020-05-29 12:52:50 +0200

    Update Friedmantest.R

--- Friedmantest.R
+++ Friedmantest.R
@@ -1,4 +1,4 @@
-anonymized <- read.csv("~/Documents/PhD/a_InteractiveMoodTracker/Data/PreStudy/anonymized.csv")
+anonymized <- read.csv("anonymized.csv")
 library(dplyr)
 x=select(anonymized, "A_1", "A_2", "A_3")
 friedman.test(data.matrix(x))
